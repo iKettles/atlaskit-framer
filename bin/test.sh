5772786e9f6d84ee1e31e9913d150bcd44eb65ec
Addison <addison@framer.com>
2019-08-08 17:53:16 +0200

    Add relative directory support

--- bin/test.sh
+++ bin/test.sh
@@ -3,23 +3,32 @@
 # Preserve newlines
 IFS=
 
-declare -a arr=("addison.framerfx")
+sourceDir=`dirname $0`
+declare -a arr=("addison.framerfx" "atlassian-avatar.framerfx" "atlassian-badge.framerfx" "atlassian-banner.framerfx" "atlassian-breadcrumbs.framerfx" "atlassian-button.framerfx" "atlassian-checkbox.framerfx" "atlassian-color.framerfx" "atlassian-datetime_picker.framerfx" "atlassian-icons.framerfx" "atlassian-lozenge.framerfx" "atlassian-modal_dialog.framerfx" "atlassian-radio.framerfx" "atlassian-range.framerfx" "atlassian-select.framerfx" "atlassian-textarea.framerfx" "atlassian-textfield.framerfx" "atlassian-toggle.framerfx" )
 
 for dir in "${arr[@]}"
 do
+  cd $dir
+
+  workingDir=$(pwd)
+
+  echo $workingDir
 
   # Cache previous yarn.lock
-  previousYarnLock=$(cat $dir/yarn.lock)
+  previousYarnLock=$(cat ./yarn.lock)
 
   # Update dependencies
-  yarn
+  yarn > /dev/null
 
   # Cache new yarn.lock
-  newYarnLock=$(cat $dir/yarn.lock)
+  newYarnLock=$(cat ./yarn.lock)
 
-  namespacedDependenciesChanged=$(node `dirname $0`/yarn-lock-diff.js @addisonkit $previousYarnLock $newYarnLock)
+  namespacedDependenciesChanged=$(node ../bin/yarn-lock-diff.js @atlaskit $previousYarnLock $newYarnLock)
 
   if [ "$namespacedDependenciesChanged" == "true" ]; then
     # package was updated
+    echo "$dir changed"
   fi
+
+  cd ..
 done
